b034fd1bed582d9404452b21f275bb4228077ffc
Vimean Chea <vimeanc@csu.fullerton.edu>
2020-07-05 16:58:11 -0700

    Updates

---
+++ precache-manifest.0b0b4cd2da99b948bd8322aeaef77bed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21a45a6c4c1b6cfdbefeb15adce72c77",
+    "url": "/DisneyPrincess/index.html"
+  },
+  {
+    "revision": "ed8e321e15ef526a2520",
+    "url": "/DisneyPrincess/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "03a8b9590a8c58cdd2d6",
+    "url": "/DisneyPrincess/static/css/main.ae0b8eb3.chunk.css"
+  },
+  {
+    "revision": "ed8e321e15ef526a2520",
+    "url": "/DisneyPrincess/static/js/2.2bdce9d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DisneyPrincess/static/js/2.2bdce9d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03a8b9590a8c58cdd2d6",
+    "url": "/DisneyPrincess/static/js/main.226879b5.chunk.js"
+  },
+  {
+    "revision": "e65071b228f2fd2b4f03",
+    "url": "/DisneyPrincess/static/js/runtime-main.896b04ce.js"
+  },
+  {
+    "revision": "6d19ff8d853a9c017740d18ba595eac5",
+    "url": "/DisneyPrincess/static/media/The Perfect Christmas.6d19ff8d.woff"
+  }
+]);
